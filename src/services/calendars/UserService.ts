6dbbd30b490fff363d92200dbdd016bf126c2f2e
NTekula <NTekula@tracfone.com>
2020-07-09 18:09:05 -0400

    changes

--- src/services/calendars/UserService.ts
+++ src/services/calendars/UserService.ts
@@ -1,15 +1,15 @@
-import { Inject, Service } from "@tsed/common";
+import { Inject, Service, Injectable } from "@tsed/common";
 import { MongooseModel } from "@tsed/mongoose";
 import { User } from "../../models/users/User";
 
 
-@Service()
+// @Service()
+@Injectable()
 export class UserService {
-  @Inject(User)
-  private User: MongooseModel<User>;
+  constructor(@Inject(User) private userModel:   MongooseModel<User>) {}
 
   async find(id: string): Promise<User> {
-    const user = await this.User.findById(id).exec();
+    const user = await this.userModel.findById(id).exec();
     return user;
   }
 
